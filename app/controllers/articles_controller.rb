82c9b9c85e72cfa29b93fb96d0b35aff82c7348c
Sabrina Jurlina <sabrina.jurlina@gmail.com>
2022-12-15 20:53:07 -0600

    added new, create, edit, update views and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,38 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    #have to create a new article instance upon page load so that form can render with no errors
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      # redirect_to article_path(@article)
+      # OR, this shorthand
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # to render object without a template/view...
+    # render plain: @article.inspect
+    # render plain: params[:article]
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
